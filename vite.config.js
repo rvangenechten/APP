d35da3f3ed8c32e8055fc1a2ba4a68848d821135
rvangenechten <rvangenechten17@gmail.com>
2025-09-07 22:04:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/APP/", // 👈 this is critical for GitHub Pages
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
   },
-})
+});
